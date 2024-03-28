e87355cf00769a543b175969942822a1b8d58e1e
“GoicO” <17802018010@163.com>
2024-03-28 12:35:19 +0800

    蛋糕商城

---
+++ CookieYun/src/listener/ApplicationListener.java
@@ -0,0 +1,72 @@
+package listener;
+
+import com.zj_tjy_zky.service.TypeService;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+import javax.servlet.http.HttpSessionAttributeListener;
+import javax.servlet.http.HttpSessionEvent;
+import javax.servlet.http.HttpSessionListener;
+import javax.servlet.http.HttpSessionBindingEvent;
+
+@WebListener()
+public class ApplicationListener implements ServletContextListener,
+        HttpSessionListener, HttpSessionAttributeListener {
+    TypeService tsService=new TypeService();
+
+    // Public constructor is required by com.zj.servlet spec
+    public ApplicationListener() {
+    }
+
+    // -------------------------------------------------------
+    // ServletContextListener implementation
+    // -------------------------------------------------------
+    public void contextInitialized(ServletContextEvent sce) {
+      /* This method is called when the com.zj.servlet context is
+         initialized(when the Web application is deployed). 
+         You can initialize com.zj.servlet context related data here.
+      */
+        sce.getServletContext().setAttribute("typeList",tsService.GetAllType());
+    }
+
+    public void contextDestroyed(ServletContextEvent sce) {
+      /* This method is invoked when the Servlet Context 
+         (the Web application) is undeployed or 
+         Application Server shuts down.
+      */
+    }
+
+    // -------------------------------------------------------
+    // HttpSessionListener implementation
+    // -------------------------------------------------------
+    public void sessionCreated(HttpSessionEvent se) {
+        /* Session is created. */
+    }
+
+    public void sessionDestroyed(HttpSessionEvent se) {
+        /* Session is destroyed. */
+    }
+
+    // -------------------------------------------------------
+    // HttpSessionAttributeListener implementation
+    // -------------------------------------------------------
+
+    public void attributeAdded(HttpSessionBindingEvent sbe) {
+      /* This method is called when an attribute 
+         is added to a session.
+      */
+    }
+
+    public void attributeRemoved(HttpSessionBindingEvent sbe) {
+      /* This method is called when an attribute
+         is removed from a session.
+      */
+    }
+
+    public void attributeReplaced(HttpSessionBindingEvent sbe) {
+      /* This method is invoked when an attibute
+         is replaced in a session.
+      */
+    }
+}
